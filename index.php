bd4c26df20dcf41820df7621959c6f046f6e5f2d
54376 <93352628+54376@users.noreply.github.com>
2022-11-02 09:25:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,8 @@
   <title>Taller: Despliegue de aplicaciones con Heroku</title>
  </head>
  <body>
-  body{
   <background-color(pink);
-  }
  <?php echo '<p>Uwu</p>'; ?>
+ "<body style='background-color:pink'>";
  </body>
 </html>
